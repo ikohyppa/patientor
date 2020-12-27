fff89ece73a883e8bc3ff1859af5dbec8d03ece6
ikohyppa <iko.hyppanen@gmail.com>
2020-12-27 14:28:45 +0200

    9.17: patientor, step2

---
+++ src/PatientInfoPage/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { Icon } from 'semantic-ui-react';
+
+import { Patient } from '../types';
+import { apiBaseUrl } from '../constants';
+import { useStateValue } from '../state';
+
+const PatientInfoPage: React.FC = () => {
+  const [{ patients }, dispatch] = useStateValue();
+
+  const { id } = useParams<{ id: string }>();
+  const patient = patients[id];
+
+  React.useEffect(() => {
+    const fetchPatientInfo = async () => {
+      try {
+        const { data: patientInfoFromApi } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        dispatch({ type: 'UPDATE_PATIENT_INFO', payload: patientInfoFromApi });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    if (patient && !patients[id].ssn) {
+      fetchPatientInfo();
+    }
+  }, [id, dispatch, patients]);
+
+  if (!patient || !patient.ssn) return null;
+
+  const genderIcon = (gender: string) => {
+    if (patient) {
+      if (gender === 'male') return <Icon name='mars' />;
+      if (gender === 'female') return <Icon name='venus' />;
+      return <Icon name='genderless' />;
+    }
+  };
+
+  return (
+    <div className='App'>
+      <h2>
+        {patient.name}
+        {genderIcon(patient.gender)}
+      </h2>
+      <p>{patient.ssn}</p>
+      <p>{patient.occupation}</p>
+    </div>
+  );
+};
+
+export default PatientInfoPage;
